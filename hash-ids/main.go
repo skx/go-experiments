56603b186a058ba5d19b30df67bd4d90e3653318
Steve Kemp <steve@steve.org.uk>
2015-09-13 13:36:42 +0300

    Updated header, reformatted

--- hash-ids/main.go
+++ hash-ids/main.go
@@ -1,22 +1,27 @@
 /**
- * See if we can has integers.
+ * See if we can hash integers, such that
+ * we could create unique "short identifiers"
+ * for uploaded files.
  */
- package main
 
-import "fmt"
-import "github.com/speps/go-hashids"
+package main
+
+import (
+	"fmt"
+	"github.com/speps/go-hashids"
+)
 
 func main() {
-    hd := hashids.NewData()
-    hd.Salt = "I hope this is secure"
-    hd.MinLength = 1
-    h := hashids.NewWithData(hd)
+	hd := hashids.NewData()
+	hd.Salt = "I hope this is secure"
+	hd.MinLength = 1
+	h := hashids.NewWithData(hd)
 
-    for i := 0; i<10000;i++ {
-      numbers := []int{99}
-      numbers[0] = i
+	for i := 0; i < 10000; i++ {
+		numbers := []int{99}
+		numbers[0] = i
 
-      e, _ := h.Encode(numbers)
-      fmt.Printf("%d -> %s\n", i ,e)
-    }
+		e, _ := h.Encode(numbers)
+		fmt.Printf("%d -> %s\n", i, e)
+	}
 }
