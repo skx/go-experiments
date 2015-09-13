db1c14e4527a3d28d4285a79a63f27af6105d881
Steve Kemp <steve@steve.org.uk>
2015-09-13 21:11:29 +0300

    Added example

---
+++ executable-source/executable-source.go
@@ -0,0 +1,13 @@
+//usr/bin/env go run $0 $@; exit
+//
+// If this source-file is marked as executable
+// then the source may be executed!
+//
+
+package main
+
+import "fmt"
+
+func main() {
+        fmt.Println("hello world")
+}
